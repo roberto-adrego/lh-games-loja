4cfd919a8b67597668c8b0e0fcc91106bb28b32f
Roberto Adrego <roberto.adrego@gmail.com>
2024-05-03 21:23:55 -0300

    new file:   .vscode/extensions.json 	new file:   .vscode/launch.json 	new file:   .vscode/tasks.json 	new file:   Golden Office.txt 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app-routing.module.ts 	modified:   src/app/app.component.spec.ts 	modified:   src/app/app.module.ts 	modified:   src/app/inicio/inicio.component.css 	modified:   src/app/inicio/inicio.component.html 	modified:   src/app/login/login.component.css 	modified:   src/app/login/login.component.html 	modified:   src/app/menu/menu.component.css 	modified:   src/app/menu/menu.component.html 	modified:   src/app/menu/menu.component.ts 	deleted:    src/app/models/user.spec.ts 	deleted:    src/app/models/user.ts 	modified:   src/app/rodape/rodape.component.css 	modified:   src/app/rodape/rodape.component.html 	modified:   src/index.html 	modified:   src/styles.css

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'loja-games'`, () => {
+  it(`should have as title 'game'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('loja-games');
+    expect(app.title).toEqual('game');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('loja-games app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('game app is running!');
   });
 });
